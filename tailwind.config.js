b379c68190266a92eb10870451a29dfaca8d1632
githubunny <131416506+nidarifda@users.noreply.github.com>
2025-10-04 22:14:25 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "media", // or 'class' if you want manual toggle
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
